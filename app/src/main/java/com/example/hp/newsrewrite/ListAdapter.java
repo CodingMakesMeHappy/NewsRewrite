47730a494a8a9f3528c91eccec9b9078da4e77ff
CodingMakesMeHappy <75936119@qq.com>
2017-02-28 22:06:49 +0800

    add something

--- app/src/main/java/com/example/hp/newsrewrite/ListAdapter.java
+++ app/src/main/java/com/example/hp/newsrewrite/ListAdapter.java
@@ -1,7 +1,11 @@
 package com.example.hp.newsrewrite;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -11,13 +15,35 @@ import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter {
     private List<ListBean> listBeen;
+    private LayoutInflater layoutInflater;
 
     ListAdapter(List<ListBean> listBeen) {
         this.listBeen = listBeen;
     }
 
+
+    public class NewsViewHold extends RecyclerView.ViewHolder {
+
+        RecyclerView list;
+        ImageView item_pic;
+        TextView item_subject;
+        TextView item_summary;
+
+        public NewsViewHold(View itemView) {
+            super(itemView);
+            list = (RecyclerView)itemView.findViewById(R.id.list);
+            item_pic = (ImageView) itemView.findViewById(R.id.item_pic);
+            item_subject = (TextView) itemView.findViewById(R.id.item_subject);
+            item_summary = (TextView) itemView.findViewById(R.id.item_summary);
+        }
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = layoutInflater.from(context).inflate(R.layout.news_item, parent, false);
+
+        return new NewsViewHold(view);
         return null;
     }
 
