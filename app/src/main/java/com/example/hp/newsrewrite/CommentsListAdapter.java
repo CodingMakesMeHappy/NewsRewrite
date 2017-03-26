ceb4bbed9fd1d008413a503c7eda7af479e075b7
CodingMakesMeHappy <75936119@qq.com>
2017-03-26 20:23:32 +0800

    add something

---
+++ app/src/main/java/com/example/hp/newsrewrite/CommentsListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.hp.newsrewrite;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by HP on 2017/3/12.
+ */
+
+public class CommentsListAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/example/hp/newsrewrite/NewsComments.java
@@ -0,0 +1,8 @@
+package com.example.hp.newsrewrite;
+
+/**
+ * Created by HP on 2017/3/11.
+ */
+
+public class NewsComments {
+}
